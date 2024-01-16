297cc6eb5e51c9f58b349cc0155f9c412711975f
leon rodriguez <leonrodriguez71@gmail.com>
2024-01-16 17:43:58 -0300

    metatags for share links

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,21 @@ import '../styles/mdStyles.css';
 import '../styles/custom.css';
 import Layout from '../components/Layout';
 import Script from 'next/script';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta property="og:title" content="Milonga City" />
+        <meta
+          property="og:description"
+          content="Discover the magic of tango with our exclusive services. Authentic classes, unforgettable events, and unique experiences in the heart of tango. Immerse yourself in the passion and elegance of the world's most passionate dance!"
+        />
+        <meta property="og:image" content="/Images/bailarines.jpg" />
+        <meta property="og:url" content="milonga.city" />
+        {/* Otras meta etiquetas si es necesario */}
+      </Head>
       <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-6X0CZ3JPJS"
