3a47633247ad4e0ba3fb405c46853cbe7414fb3a
leon rodriguez <leonrodriguez71@gmail.com>
2024-03-04 16:38:21 -0300

    translation for footer and fixed nav styles on mobile

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import logo from '../assets/logo-milonga-city.svg';
 import Image from 'next/image';
+import { useTranslation } from 'react-i18next';
 import { FaFacebookF, FaTwitter, FaTripadvisor } from 'react-icons/fa';
 import { FaWhatsapp } from 'react-icons/fa';
 import { IoMailOutline } from 'react-icons/io5';
@@ -7,48 +8,48 @@ import { MdOutlinePlace } from 'react-icons/md';
 import { RiInstagramFill } from 'react-icons/ri';
 
 const Footer = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="w-full">
       <div className=" min-[1300px]:px-[100px] bg-[#0088cc] h-96 grid grid-cols-4 max-lg:grid-cols-2 max-lg:grid-rows-2 max-lg:min-h-[750px] max-sm:grid-cols-1 max-sm:grid-rows-[2fr_2fr_1fr_2fr] max-sm:min-h-[1000px]">
         <div className="grid grid-rows-[128px_auto]  max-sm:pl-0 max-sm:grid-rows-[80px_auto]">
           <div className="flex justify-center items-center text-3xl font-bold text-white max-sm:justify-center">
-            About us
+            {t('footer_aboutus')}
           </div>
           <div className="flex justify-center max-sm:justify-center">
             <div className="w-56 text-white font-bold leading-7">
-              We are dedicated to curating exceptional travel experiences that
-              blend the excitement of discovering new landscapes with the allure
-              of the tango rhythm.
+              {t('footer_aboutus_body')}
             </div>
           </div>
         </div>
         <div className="grid grid-rows-[128px_auto]  max-sm:pl-0 max-sm:grid-rows-[80px_auto]">
           <div className="flex justify-center items-center text-3xl font-bold text-white max-sm:justify-center">
-            Activities
+            {t('footer_activities')}
           </div>
           <div className="flex justify-center max-sm:justify-center">
             <ul className="list-disc text-white font-bold list-inside leading-8">
-              <li>
-                <a>Milongas Visit Excursion</a>
+              <li className="max-sm:flex justify-center">
+                <a>{t('footer_activities_item1')}</a>
               </li>
-              <li>
-                <a>Tango classes</a>
+              <li className="max-sm:flex justify-center">
+                <a>{t('footer_activities_item2')}</a>
               </li>
-              <li>
-                <a>Tango lesson + Wine Tasting</a>
+              <li className="max-sm:flex justify-center">
+                <a>{t('footer_activities_item3')}</a>
               </li>
-              <li>
-                <a>Group Tango Class + Afternoon Snack</a>
+              <li className="max-sm:flex justify-center">
+                <a>{t('footer_activities_item4')}</a>
               </li>
-              <li>
-                <a>Dinner and Tango Show</a>
+              <li className="max-sm:flex justify-center">
+                <a>{t('footer_activities_item5')}</a>
               </li>
             </ul>
           </div>
         </div>
         <div className="grid grid-rows-[128px_auto] max-sm:pl-0 max-sm:grid-rows-[80px_auto]">
           <div className="flex justify-center items-center text-3xl font-bold text-white max-sm:justify-center">
-            Follow Us
+            {t('footer_followus')}
           </div>
           <div className="flex justify-center pb-10 max-sm:justify-center">
             <div className="text-white w-40 text-xl font-bold flex justify-center space-x-7">
@@ -75,7 +76,7 @@ const Footer = () => {
         </div>
         <div className="grid grid-rows-[128px_auto] max-sm:pl-0 max-sm:grid-rows-[80px_auto]">
           <div className="flex justify-center items-center text-3xl font-bold text-white max-sm:justify-center">
-            Contact
+            {t('footer_contact')}
           </div>
           <div className="flex justify-center mt-1 leading-8 text-white font-bold max-sm:justify-center">
             <ul className="space-y-4">
