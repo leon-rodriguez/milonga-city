b05e0c4be321f18a4bb3d311060b5b71a6240968
leon rodriguez <leonrodriguez71@gmail.com>
2025-03-26 12:08:21 -0300

    changed color spinner

--- components/LoaderSpinner.js
+++ components/LoaderSpinner.js
@@ -1,7 +1,7 @@
 const LoaderSpinner = ({ size }) => {
   return (
     <div
-      className={`mt-8 ml-8 w-[100px] h-[100px] rounded-full border-4 border-[rgba(0, 0, 0, .1)] border-l-[#0088cc] animate-[spin_1s_linear_infinite]`}
+      className={`mt-8 ml-8 w-[100px] h-[100px] rounded-full border-4 border-[rgba(0, 0, 0, .1)] border-l-primary animate-[spin_1s_linear_infinite]`}
     ></div>
   );
 };
